45091c0a4ef8a432219833bb39db92494df75d80
Nanle Paul <paulnanle611@gmail.com>
2019-02-20 23:44:43 +0100

    created mysql database and models for Users and Expenses tables

--- ExpenseServer/database/migrations/2019_02_20_223655_create_expenses_table.php
+++ ExpenseServer/database/migrations/2019_02_20_223655_create_expenses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateExpensesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('expenses', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->string('expense_date', 30);
+            $table->double('value',20,2);
+            $table->text('reason_expense');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('expenses');
     }
 }
